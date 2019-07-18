5784191429ead8419f59ac86dc98a629f2d5096d
Chris Applegate <github@qwghlm.co.uk>
2019-07-18 14:35:54 -0400

    Convert to use React hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 const postcodeRegex = /^[A-Z]{1,2}[0-9]{1,2}[A-Z]?\s?[0-9][A-Z]{2}$/i;
 
-class App extends Component {
+function App() {
 
-  state = {
-    postcodeValue: "",
-    buttonEnabled: false,
+  const [postcodeValue, setPostcodeValue] = useState("");
+  const [buttonEnabled, setButtonEnabled] = useState(false);
+
+  const onChange = (e) => {
+    const postcode = e.currentTarget.value;
+    setPostcodeValue(postcode);
+    setButtonEnabled(postcode.match(postcodeRegex));
   }
 
-  onChange = (e) => {
-    this.setState({
-      postcodeValue: e.currentTarget.value,
-      buttonEnabled: e.currentTarget.value.match(postcodeRegex),
-    });
-  }
-
-  onSearchSubmit = (e) => {
+  const onSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
+    console.log(postcodeValue);
   }
 
-  render() {
-    const { postcodeValue, buttonEnabled } = this.state;
-    return (
-      <div className="App row">
+  return (
+    <div className="App row">
 
-        <h1>Postcode checker</h1>
+      <h1>Postcode checker</h1>
 
-        <p>To find out more about a postcode, enter one below and clock "Search"</p>
+      <p>To find out more about a postcode, enter one below and clock "Search"</p>
 
-        <form action="#" className="col s12" onSubmit={this.onSearchSubmit}>
-          <div className="input-field col s10">
-            <input type="text" placeholder="Enter a postcode" name="postcode"
-              value={postcodeValue} onChange={this.onChange} />
-          </div>
-          <div className="input-field col s2">
-            <button className="btn" disabled={!buttonEnabled}>Search</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+      <form action="#" className="col s12" onSubmit={onSearchSubmit}>
+        <div className="input-field col s10">
+          <input type="text" placeholder="Enter a postcode" name="postcode"
+            value={postcodeValue} onChange={onChange} />
+        </div>
+        <div className="input-field col s2">
+          <button className="btn" disabled={!buttonEnabled}>Search</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
