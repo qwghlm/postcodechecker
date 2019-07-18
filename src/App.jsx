ff7df63ff98d2303127ee7239c25e4a4397800fa
Chris Applegate <github@qwghlm.co.uk>
2019-07-18 14:28:22 -0400

    A little streamlining

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.scss';
 
 const postcodeRegex = /^[A-Z]{1,2}[0-9]{1,2}[A-Z]?\s?[0-9][A-Z]{2}$/i;
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App.jsx';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // TODO: Service worker
-serviceWorker.unregister();
+// import * as serviceWorker from './serviceWorker';
+// serviceWorker.unregister();
