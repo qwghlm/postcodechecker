44e50fdaabc3eec0afabe300bf2ed2dfd605d0b4
Chris Applegate <github@qwghlm.co.uk>
2019-07-19 17:52:41 -0400

    Add places and join

--- db/build.py
+++ db/build.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import csv
 from datetime import datetime
+import glob
 import resource
 import sys
 from pyproj import Transformer
@@ -12,8 +13,26 @@ ONSPD_FILE = "./src/ONSPD_MAY_2019_UK.csv"
 bng_to_wgs84 = Transformer.from_crs("epsg:27700", "epsg:4326")
 ing_to_wgs84 = Transformer.from_crs("epsg:29903", "epsg:4326")
 
+PLACES_FILES = {"westminster": "WPC"}
 
-def run():
+
+def build_places():
+    places_fixture = open("./fixtures/places.csv", "w")
+    places = csv.DictWriter(places_fixture, ["id", "name", "type"])
+    places.writeheader()
+
+    for filename in PLACES_FILES.keys():
+
+        reader = csv.DictReader(open(f"./src/places/{filename}.csv"))
+        for line in reader:
+            line["type"] = PLACES_FILES[filename]
+            places.writerow(line)
+
+    places_fixture.flush()
+    places_fixture.close()
+
+
+def build_postcodes():
     """
     Parses the national postcode file into a set of database fixtures
     """
@@ -30,6 +49,12 @@ def run():
         postcodes_fixture, ["id", "postcode", "latitude", "longitude"]
     )
     postcodes.writeheader()
+
+    postcodes_places_fixture = open("fixtures/postcodes__places.csv", "w")
+    postcodes_places = csv.DictWriter(
+        postcodes_places_fixture, ["postcode_id", "place_id"]
+    )
+    postcodes_places.writeheader()
 
     count = 0
     for line in reader:
@@ -55,6 +80,9 @@ def run():
                 "longitude": "{0:.6f}".format(longitude),
             }
         )
+        postcodes_places.writerow(
+            {"postcode_id": normalized_postcode, "place_id": line["pcon"]}
+        )
 
         count += 1
         if count % 100 == 0:
@@ -66,9 +94,16 @@ def run():
 
     postcodes_fixture.flush()
     postcodes_fixture.close()
+    postcodes_places_fixture.flush()
+    postcodes_places_fixture.close()
 
     print(f"\n{count} postcodes written")
     print(f"Time taken: {(datetime.now() - start_time)}")
+
+
+def run():
+    build_places()
+    build_postcodes()
 
 
 if __name__ == "__main__":
